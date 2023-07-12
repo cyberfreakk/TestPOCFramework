14a581d1ae6572084e916c5df83655d4bfd81208
Arunesh Gupta <arunesh.gupta@globallogic.com>
2023-07-12 12:26:14 +0530

    Finished our TestFramework

---
+++ TestPOCFramework/TestPOCFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TestPOCFramework
+//
+//  Created by global on 12/07/23.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init() {  }
+    
+    public static func doSomething() -> String{
+        return "This is test framework!"
+    }
+}
---
+++ TestPOCFramework/TestPOCFramework/TestPOCFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestPOCFramework.h
+//  TestPOCFramework
+//
+//  Created by global on 12/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPOCFramework.
+FOUNDATION_EXPORT double TestPOCFrameworkVersionNumber;
+
+//! Project version string for TestPOCFramework.
+FOUNDATION_EXPORT const unsigned char TestPOCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPOCFramework/PublicHeader.h>
+
+
